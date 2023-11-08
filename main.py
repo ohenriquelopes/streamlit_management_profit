38fb31511b7688660103f6b6d348cbe6e66e3d7e
henrique <hlopessantos52@gmail.com>
2023-11-08 17:02:37 -0300

    clear main.py

--- main.py
+++ main.py
@@ -13,35 +13,10 @@ def main():
     st.markdown('<link rel="stylesheet" type="text/css" href="custom.css">', unsafe_allow_html=True)
 
 
-    # func_all.get_contas_fixas()
-    # func_all.get_salarios_history()
-    # func_all.get_equivalente()
-    #
-    # func_all.get_soma_por_mes()
-
-    # idk = func_all.get_soma_por_mes()
-    # idk2 = func_all.get_equivalente()
-
-    # func_all.get_equivalente_calculado()
-
-    # idk2['resultado'] = idk['entrada'] * (idk2['Porcentagem'] / 100)
-    # st.dataframe(idk2)
-
-    # idk2 = func_all.calcular_e_adicionar_coluna(idk, idk2)
-    # st.dataframe(idk2)
-
-    # st.sidebar.header("Menu")
-    # conn = sqlite3.connect('my_data_base.db')
-    # cursor = conn.cursor()
-
-    #
-    # df = func_all.query_rateio()
-    # st.write(df)
-    # df1 = df
-    # st.write(df1[0])
-    # st.write(df1[0][2])
     func_all.get_rateio_mes()
     func_all.get_mes_atual()
+
+
 
 if __name__ == '__main__':
     main()
